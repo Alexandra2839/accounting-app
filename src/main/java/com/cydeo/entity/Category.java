84a7ff7261d36805f75d84f0a07f6307ea691b3b
stanislavpavlovskyi <lichfb@gmail.com>
2023-06-26 23:19:20 -0400

    Category Entity created

--- src/main/java/com/cydeo/entity/Category.java
+++ src/main/java/com/cydeo/entity/Category.java
@@ -1,9 +1,22 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "categories")
 public class Category extends BaseEntity {
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "company_id")
+    private Company company;
 }
