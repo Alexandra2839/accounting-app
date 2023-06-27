bcbd66db9de1f4875c2adb442faa53328721094c
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-06-27 12:15:10 -0400

    ProductRepository added

---
+++ src/main/java/com/cydeo/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Long, Product> {
+}
