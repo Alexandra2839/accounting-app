ea0e9c82549006f1ccf69897f672129e5c174b15
Anna Pierre-Louis <anitataksa3440@gmail.com>
2023-06-27 17:01:05 -0400

    L-18 Created "CompanyRepository" interface              "CompanyService" interface              "CompanyServiceImpl" class from ticket L-15      Created "CompanyDTOConverter"

---
+++ src/main/java/com/cydeo/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,37 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.CompanyDto;
+import com.cydeo.entity.Company;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.CompanyRepository;
+import com.cydeo.service.CompanyService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+    private final MapperUtil mapperUtil;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository, MapperUtil mapperUtil) {
+        this.companyRepository = companyRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+
+    @Override
+    public CompanyDto findById(long id) {
+
+        Optional<Company> company = companyRepository.findById(id);
+
+        if(company.isPresent()){
+            return mapperUtil.convert(company.get(), new CompanyDto());
+        }
+        return null;
+
+    }
+}
