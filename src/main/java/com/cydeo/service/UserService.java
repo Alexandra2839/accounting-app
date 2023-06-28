b42c9ff3d6ba0fd916bc1e20abac6d91d67172c9
Sasha <alexandra.aslobodian@gmail.com>
2023-06-27 22:08:25 -0400

    added and implemented findById, ListAllUsers, update and save methods

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -2,8 +2,18 @@ package com.cydeo.service;
 
 import com.cydeo.dto.UserDto;
 
+import java.util.List;
+
 public interface UserService {
 
 
     UserDto findByUsername(String username);
+
+    UserDto findById(Long id);
+
+    List<UserDto> listAllUsers();
+
+    UserDto update(UserDto user);
+
+    UserDto save(UserDto user);
 }
--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDto;
+import com.cydeo.entity.User;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -12,13 +18,58 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final MapperUtil mapperUtil;
 
-    public UserServiceImpl(UserRepository userRepository, MapperUtil mapperUtil) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, MapperUtil mapperUtil, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.mapperUtil = mapperUtil;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserDto findByUsername(String username) {
         return mapperUtil.convert(userRepository.findByUsername(username), new UserDto());
+    }
+
+    @Override
+    public UserDto findById(Long id) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("User with id " + id + "could not be found"));
+        return mapperUtil.convert(user, new UserDto());
+    }
+
+    @Override
+    public List<UserDto> listAllUsers() {
+        return userRepository.findAll().stream()
+                .map(user -> mapperUtil.convert(user, new UserDto()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto update(UserDto user) {
+
+
+        User user1 = userRepository.findByUsername(user.getUsername());
+
+        User convertedUser = mapperUtil.convert(user, new User());   // has id?
+
+        convertedUser.setId(user1.getId());
+
+        userRepository.save(convertedUser);
+
+        return findByUsername(user.getUsername());
+
+    }
+
+    @Override
+    public UserDto save(UserDto user) {
+        user.setEnabled(true);
+
+        User obj = mapperUtil.convert(user, new User());
+        obj.setPassword(passwordEncoder.encode(obj.getPassword()));
+
+        userRepository.save(obj);
+        return mapperUtil.convert(obj, new UserDto());
     }
 }
