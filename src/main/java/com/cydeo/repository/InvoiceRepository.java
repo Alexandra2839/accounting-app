3797b48b8c0ba010407c6a4509475546c47355f2
stanislavpavlovskyi <lichfb@gmail.com>
2023-06-28 13:53:46 -0400

    Invoice service, repository, serviceImpl created

---
+++ src/main/java/com/cydeo/repository/InvoiceRepository.java
@@ -0,0 +1,11 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
+
+    List<Invoice> findAllByInvoiceNoStartingWith(String s);
+}
---
+++ src/main/java/com/cydeo/service/impl/InvoiceServiceImpl.java
@@ -0,0 +1,41 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.InvoiceDto;
+import com.cydeo.entity.Invoice;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.InvoiceRepository;
+import com.cydeo.service.InvoiceService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@Service
+public class InvoiceServiceImpl implements InvoiceService {
+    private final InvoiceRepository invoiceRepository;
+    private final MapperUtil mapperUtil;
+
+    public InvoiceServiceImpl(InvoiceRepository invoiceRepository, MapperUtil mapperUtil) {
+        this.invoiceRepository = invoiceRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public InvoiceDto findById(Long id) {
+        Invoice invoice = invoiceRepository.findById(id).orElseThrow();
+        return mapperUtil.convert(invoice, new InvoiceDto());
+    }
+
+    @Override
+    public List<InvoiceDto> listOfAllInvoices() {
+        List<Invoice> all = invoiceRepository.findAll();
+        return  all.stream().map(invoice -> mapperUtil.convert(invoice, new InvoiceDto())).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<InvoiceDto> listOfPurchasedInvoices(String s) {
+        List<Invoice> allPurchased = invoiceRepository.findAllByInvoiceNoStartingWith(s);
+        return allPurchased.stream().map(invoice -> mapperUtil.convert(invoice, new InvoiceDto())).collect(Collectors.toList());
+    }
+}
