6238e447e4dd7bb99aa69f1bc0df9cdb2c77c4c6
Anna Pierre-Louis <anitataksa3440@gmail.com>
2023-06-29 16:28:16 -0400

    L-23 added findAllBesidesId1OrderedByStatusAndTitle() method to CompanyRepository

--- src/main/java/com/cydeo/repository/CompanyRepository.java
+++ src/main/java/com/cydeo/repository/CompanyRepository.java
@@ -2,7 +2,14 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CompanyRepository extends JpaRepository<Company,Long> {
+
+    @Query(value = "SELECT * FROM companies WHERE id != 1 ORDER BY company_status, title ASC", nativeQuery = true)
+    List<Company> findAllBesidesId1OrderedByStatusAndTitle();
+
 
 }
