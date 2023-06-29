11350a7b4b49b32bb2ed9d91d0c0f38add1b1fdf
lichfb <lichfb@gmail.com>
2023-06-28 21:07:36 -0400

    @Where clause added for deletion function

--- src/main/java/com/cydeo/entity/Category.java
+++ src/main/java/com/cydeo/entity/Category.java
@@ -4,6 +4,7 @@ import com.cydeo.entity.common.BaseEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 
@@ -12,6 +13,7 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor
 @Table(name = "categories")
+@Where(clause = "is_deleted=false")
 public class Category extends BaseEntity {
 
     private String description;
