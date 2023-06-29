77fd5130b9f95f3b25d499e8873d686ba312ecff
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-06-28 23:17:18 -0400

    ProductServiceImpl added

---
+++ src/main/java/com/cydeo/service/impl/ProductServiceImpl.java
@@ -0,0 +1,66 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProductDto;
+import com.cydeo.entity.Product;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.ProductRepository;
+import com.cydeo.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final MapperUtil mapper;
+
+    public ProductServiceImpl(ProductRepository productRepository, MapperUtil mapper) {
+        this.productRepository = productRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public ProductDto createProduct(ProductDto productDto) {
+        Product product=mapper.convert(productDto,new Product());
+        productRepository.save(product);
+        return mapper.convert(product,new ProductDto());
+    }
+
+    @Override
+    public List<ProductDto> listAllProducts() {
+        List<Product> productList=productRepository.findAll();
+        return productList.stream()
+                .map(p->mapper.convert(p,new ProductDto()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ProductDto findProductById(Long id) {
+        Product product= productRepository.findByIdAndIsDeleted(id,false);
+
+        return mapper.convert(product,new ProductDto());
+    }
+
+    @Override
+    public ProductDto updateProduct(ProductDto productDto) {
+        Product product=mapper.convert(productDto,new Product());
+        product.setId(productDto.getId());
+        productRepository.save(product);
+
+        return findProductById(productDto.getId());
+    }
+
+    @Override
+    public ProductDto deleteProduct(ProductDto productDto) {
+        Product product=productRepository.findByIdAndIsDeleted(productDto.getId(),false);
+        product.setIsDeleted(true);
+        product.setName(productDto.getName()+" / "+productDto.getId());
+        productRepository.save(product);
+
+        return mapper.convert(product,new ProductDto());
+    }
+
+
+}
