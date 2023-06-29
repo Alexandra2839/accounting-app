21162c6a29520250d70e890cb46f98396d6400c3
lichfb <lichfb@gmail.com>
2023-06-28 20:20:58 -0400

    CRUD function created

--- src/main/java/com/cydeo/service/ClientVendorService.java
+++ src/main/java/com/cydeo/service/ClientVendorService.java
@@ -2,6 +2,14 @@ package com.cydeo.service;
 
 import com.cydeo.dto.ClientVendorDto;
 
+
+import java.util.List;
+
 public interface ClientVendorService {
     ClientVendorDto findById(Long id);
+    List<ClientVendorDto> findAll();
+
+    ClientVendorDto save(ClientVendorDto clientVendorDto);
+    ClientVendorDto update(ClientVendorDto clientVendorDto);
+    ClientVendorDto delete(ClientVendorDto clientVendorDto);
 }
--- src/main/java/com/cydeo/service/impl/ClientVendorImpl.java
+++ src/main/java/com/cydeo/service/impl/ClientVendorImpl.java
@@ -7,7 +7,9 @@ import com.cydeo.repository.ClientVendorRepository;
 import com.cydeo.service.ClientVendorService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientVendorImpl implements ClientVendorService {
@@ -22,6 +24,39 @@ public class ClientVendorImpl implements ClientVendorService {
     @Override
     public ClientVendorDto findById(Long id) {
         ClientVendor clientVendor = clientVendorRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+        return mapperUtil.convert(clientVendor, new ClientVendorDto());
+    }
+
+    @Override
+    public List<ClientVendorDto> findAll() {
+        List<ClientVendor> listOfCV = clientVendorRepository.findAll();
+        return listOfCV.stream().map(CV -> mapperUtil.convert(CV,new ClientVendorDto())).collect(Collectors.toList());
+    }
+
+    @Override
+    public ClientVendorDto save(ClientVendorDto clientVendorDto) {
+        ClientVendor convertedCV = mapperUtil.convert(clientVendorDto, new ClientVendor());
+        clientVendorRepository.save(convertedCV);
+        return mapperUtil.convert(convertedCV, new ClientVendorDto());
+    }
+
+    @Override
+    public ClientVendorDto update(ClientVendorDto clientVendorDto) {
+        ClientVendor clientVendorInDB = clientVendorRepository.findById(clientVendorDto.getId()).orElseThrow();
+        ClientVendor convertedCV = mapperUtil.convert(clientVendorDto, new ClientVendor());
+
+        convertedCV.setId(clientVendorInDB.getId());
+
+        clientVendorRepository.save(convertedCV);
+        return findById(clientVendorDto.getId());
+    }
+
+    @Override
+    public ClientVendorDto delete(ClientVendorDto clientVendorDto) {
+        ClientVendor clientVendor = clientVendorRepository.findByIdAndIsDeleted(clientVendorDto.getId(), false);
+        clientVendor.setIsDeleted(true);
+        clientVendor.setClientVendorName(clientVendorDto.getClientVendorName() + " - " + clientVendorDto.getId());
+        clientVendorRepository.save(clientVendor);
         return mapperUtil.convert(clientVendor, new ClientVendorDto());
     }
 }
