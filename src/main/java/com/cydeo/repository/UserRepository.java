6f9e429dd57d10d7051f860f9b11d8d84a982769
Sasha <alexandra.aslobodian@gmail.com>
2023-06-29 12:48:12 -0400

    refactored code

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUsername (String username);
+    User findByUsername(String username);
 }
