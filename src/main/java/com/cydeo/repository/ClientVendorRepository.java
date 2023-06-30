846471e727360617fd7b2d4f4acd0a34d161cc8d
Cemil <chrisw@cydeo.com>
2023-06-30 13:24:59 +0300

    sorted client vendors and filtered by company of logged in user

--- src/main/java/com/cydeo/repository/ClientVendorRepository.java
+++ src/main/java/com/cydeo/repository/ClientVendorRepository.java
@@ -1,11 +1,18 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.ClientVendor;
+import com.cydeo.enums.ClientVendorType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ClientVendorRepository extends JpaRepository<ClientVendor, Long> {
 
+    @Query("SELECT cv FROM ClientVendor cv WHERE cv.company.title = ?1 ORDER BY cv.clientVendorType, cv.clientVendorName")
+    List<ClientVendor> findAllByCompanyTitleAndSort(String companyTitle);
+
+    @Query("SELECT cv FROM ClientVendor cv WHERE cv.company.title = ?1 and cv.clientVendorType = ?2 ORDER BY cv.clientVendorName")
+    List<ClientVendor> findAllByTypeAndSort(String companyTitle, ClientVendorType type);
 
 }
--- src/main/java/com/cydeo/service/impl/ClientVendorServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ClientVendorServiceImpl.java
@@ -2,9 +2,12 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.ClientVendorDto;
 import com.cydeo.entity.ClientVendor;
+import com.cydeo.entity.Company;
+import com.cydeo.enums.ClientVendorType;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.ClientVendorRepository;
 import com.cydeo.service.ClientVendorService;
+import com.cydeo.service.CompanyService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,49 +17,65 @@ import java.util.stream.Collectors;
 @Service
 public class ClientVendorServiceImpl implements ClientVendorService {
     private final ClientVendorRepository clientVendorRepository;
+    private final CompanyService companyService;
     private final MapperUtil mapperUtil;
 
-    public ClientVendorServiceImpl(ClientVendorRepository clientVendorRepository, MapperUtil mapperUtil) {
+    public ClientVendorServiceImpl(ClientVendorRepository clientVendorRepository, CompanyService companyService, MapperUtil mapperUtil) {
         this.clientVendorRepository = clientVendorRepository;
+        this.companyService = companyService;
         this.mapperUtil = mapperUtil;
     }
 
     @Override
     public ClientVendorDto findById(Long id) {
-        ClientVendor clientVendor = clientVendorRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No Client or Vendor founded"));
+        ClientVendor clientVendor = clientVendorRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("No Client or Vendor founded"));
         return mapperUtil.convert(clientVendor, new ClientVendorDto());
     }
 
     @Override
     public List<ClientVendorDto> findAll() {
-        List<ClientVendor> listOfCV = clientVendorRepository.findAll();
-        return listOfCV.stream().map(CV -> mapperUtil.convert(CV, new ClientVendorDto())).collect(Collectors.toList());
+        List<ClientVendor> listOfCV = clientVendorRepository
+                .findAllByCompanyTitleAndSort(companyService.getCompanyDtoByLoggedInUser().getTitle());
+        return listOfCV.stream()
+                .map(cv -> mapperUtil.convert(cv, new ClientVendorDto()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ClientVendorDto> findAllByType(ClientVendorType type) {
+        return clientVendorRepository.findAllByTypeAndSort(companyService.getCompanyDtoByLoggedInUser().getTitle(), type)
+                .stream()
+                .map(cv -> mapperUtil.convert(cv, new ClientVendorDto()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public ClientVendorDto save(ClientVendorDto clientVendorDto) {
         ClientVendor convertedCV = mapperUtil.convert(clientVendorDto, new ClientVendor());
+        convertedCV.setCompany(mapperUtil.convert(companyService.getCompanyDtoByLoggedInUser(), new Company()));
         clientVendorRepository.save(convertedCV);
         return mapperUtil.convert(convertedCV, new ClientVendorDto());
     }
 
     @Override
     public ClientVendorDto update(ClientVendorDto clientVendorDto) {
-        ClientVendor clientVendorInDB = clientVendorRepository.findById(clientVendorDto.getId()).orElseThrow(() -> new NoSuchElementException("No Client or Vendor founded"));
+        ClientVendor clientVendorInDB = clientVendorRepository.findById(clientVendorDto.getId())
+                .orElseThrow(() -> new NoSuchElementException("No Client or Vendor founded"));
         ClientVendor convertedCV = mapperUtil.convert(clientVendorDto, new ClientVendor());
 
         convertedCV.setId(clientVendorInDB.getId());
-
-        clientVendorRepository.save(convertedCV);
-        return findById(clientVendorDto.getId());
+        convertedCV.setCompany(clientVendorInDB.getCompany());
+        ClientVendor saved = clientVendorRepository.save(convertedCV);
+        return mapperUtil.convert(saved, new ClientVendorDto());
     }
 
     @Override
-    public ClientVendorDto delete(ClientVendorDto clientVendorDto) {
-        ClientVendor clientVendor = clientVendorRepository.findById(clientVendorDto.getId()).orElseThrow(() -> new NoSuchElementException("No Client or Vendor founded"));
+    public void delete(ClientVendorDto clientVendorDto) {
+        ClientVendor clientVendor = clientVendorRepository.findById(clientVendorDto.getId())
+                .orElseThrow(() -> new NoSuchElementException("No Client or Vendor founded"));
         clientVendor.setIsDeleted(true);
         clientVendor.setClientVendorName(clientVendorDto.getClientVendorName() + " - " + clientVendorDto.getId());
         clientVendorRepository.save(clientVendor);
-        return mapperUtil.convert(clientVendor, new ClientVendorDto());
     }
 }
