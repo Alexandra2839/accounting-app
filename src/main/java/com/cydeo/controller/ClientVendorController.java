f1731be190d001296e555855aeecd1b7cc3f5671
Cemil <chrisw@cydeo.com>
2023-06-30 15:27:58 +0300

    added validation to update client vendors added docker-compose.yaml file refactored controller

--- src/main/java/com/cydeo/controller/ClientVendorController.java
+++ src/main/java/com/cydeo/controller/ClientVendorController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 @Controller
@@ -29,27 +28,20 @@ public class ClientVendorController {
 
     @GetMapping("/create")
     public String showCreateClientVendorForm(Model model) {
-
-        ClientVendorType[] enumValues = ClientVendorType.values();
-        ArrayList<ClientVendorType> list = new ArrayList<>(Arrays.asList(enumValues));
-
-
         model.addAttribute("newClientVendor", new ClientVendorDto());
-        model.addAttribute("clientVendorTypes", list);
         return "/clientVendor/clientVendor-create";
 
     }
 
     @PostMapping("/create")
     public String createClientVendor(@Valid @ModelAttribute("newClientVendor") ClientVendorDto clientVendorDto,
-                                     BindingResult bindingResult, Model model) {
+                                     BindingResult bindingResult) {
 
         boolean isClientVendorNameExist = clientVendorService.isClientVendorExist(clientVendorDto);
         if (isClientVendorNameExist) {
             bindingResult.rejectValue("clientVendorName", " ", "This client-vendor already exists.");
         }
         if (bindingResult.hasErrors()) {
-            model.addAttribute("clientVendorTypes", Arrays.asList(ClientVendorType.values()));
             return "clientVendor/clientVendor-create";
         }
         clientVendorService.save(clientVendorDto);
@@ -60,15 +52,21 @@ public class ClientVendorController {
     @GetMapping("/update/{id}")
     public String editClientVendor(@PathVariable Long id, Model model) {
         model.addAttribute("clientVendor", clientVendorService.findById(id));
-        model.addAttribute("clientVendorTypes", ClientVendorType.values());
-
         return "/clientVendor/clientVendor-update";
     }
 
     @PostMapping("/update/{id}")
-    public String updateClientVendor(@PathVariable Long id, @ModelAttribute("clientVendor") ClientVendorDto clientVendorDto, Model model) {
-        model.addAttribute("clientVendor", clientVendorService.findById(id));
+    public String updateClientVendor(@PathVariable Long id,
+                                     @Valid @ModelAttribute("clientVendor") ClientVendorDto clientVendorDto,
+                                     BindingResult bindingResult) {
 
+        boolean isClientVendorNameExist = clientVendorService.isClientVendorExist(clientVendorDto);
+        if (isClientVendorNameExist) {
+            bindingResult.rejectValue("clientVendorName", " ", "This client-vendor already exists.");
+        }
+        if (bindingResult.hasErrors()) {
+            return "clientVendor/clientVendor-update";
+        }
 
         clientVendorService.update(clientVendorDto);
         return "redirect:/clientVendors/list";
@@ -78,5 +76,11 @@ public class ClientVendorController {
     public String deleteClientVender(@PathVariable Long id) {
         clientVendorService.delete(clientVendorService.findById(id));
         return "redirect:/clientVendors/list";
+    }
+
+    @ModelAttribute
+    public void commonAttributes(Model model) {
+        model.addAttribute("clientVendorTypes", Arrays.asList(ClientVendorType.values()));
+        model.addAttribute("title", "Cydeo Accounting-Client&Vendor");
     }
 }
