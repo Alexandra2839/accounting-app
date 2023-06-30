ef370b4c9ffb0b29ae2aa0d5ee1672c7388754cd
Sasha <alexandra.aslobodian@gmail.com>
2023-06-30 12:13:46 -0400

    added findAllByCompanyTitle query

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAllByOrderByCompanyTitleAscRoleDescriptionAsc();
 
     List<User> findAllByRoleDescription (String roleDescription);
+
+    List<User> findAllByCompanyTitle(String companyTitle);
 }
