a6608fc87b5a014c59360ee783e53dc329921ee2
Sasha <alexandra.aslobodian@gmail.com>
2023-06-30 16:03:18 -0400

    added findAllByDescription and findAllByDescriptionNot queries

--- src/main/java/com/cydeo/repository/RoleRepository.java
+++ src/main/java/com/cydeo/repository/RoleRepository.java
@@ -3,6 +3,12 @@ package com.cydeo.repository;
 import com.cydeo.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    List<Role> findAllByDescription(String description);
+
+    List<Role> findAllByDescriptionNot(String description);
 
 }
