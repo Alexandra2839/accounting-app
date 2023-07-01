8a8d468339b59bd3ec91c38849baa243d0874070
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-07-01 17:19:54 -0400

    isNameExist() method implementation added

--- src/main/java/com/cydeo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProductServiceImpl.java
@@ -8,8 +8,10 @@ import com.cydeo.repository.ProductRepository;
 import com.cydeo.service.CompanyService;
 import com.cydeo.service.ProductService;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,10 +37,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<ProductDto> listAllProducts() {
 
-       List<Product> list=productRepository
-               .findAllByCompanyAndProductNameSort(companyService.getCompanyDtoByLoggedInUser().getId());
+        List<Product> list = productRepository
+                .findAllByCompanyAndProductNameSort(companyService.getCompanyDtoByLoggedInUser().getId());
 
-       return list.stream().map(p->mapper.convert(p,new ProductDto())).collect(Collectors.toList());
+        return list.stream().map(p -> mapper.convert(p, new ProductDto())).collect(Collectors.toList());
     }
 
     @Override
@@ -68,6 +70,16 @@ public class ProductServiceImpl implements ProductService {
         productRepository.save(product);
 
         return mapper.convert(product, new ProductDto());
+    }
+
+    @Override
+    public boolean isNameExist(ProductDto productDto) {
+
+        Product product = productRepository.findProductByName(productDto.getName()).orElse(null);
+
+        if (product == null) return false;
+
+        return !Objects.equals(product.getId(), productDto.getId());
     }
 
 
