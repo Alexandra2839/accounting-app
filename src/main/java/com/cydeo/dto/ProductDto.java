d8986a7cc3bca461bd13b24fab7d18ec2e4b7fde
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-07-01 17:20:40 -0400

    codes refactored

--- src/main/java/com/cydeo/dto/ProductDto.java
+++ src/main/java/com/cydeo/dto/ProductDto.java
@@ -16,21 +16,21 @@ public class ProductDto {
     private Long id;
 
     @NotBlank(message = "Product Name is required field.")
-    @Size(min = 2,max=100)
+    @Size(min = 2, max = 100)
     private String name;
 
     private Integer quantityInStock;
 
-    @NotNull(message ="Low Limit Alert is a required field."
-    + " Low Limit Alert should be at least 1.")
+    @NotNull(message = "Low Limit Alert is a required field."
+            + " Low Limit Alert should be at least 1.")
     private Integer lowLimitAlert;
 
-    @NotNull(message = "Please select a Product Unit."+
-             "Product Unit is a required field.")
+    @NotNull(message = "Please select a Product Unit." +
+            "Product Unit is a required field.")
     private ProductUnit productUnit;
 
 
-    @NotNull(message = "Please select a category."+
+    @NotNull(message = "Please select a category." +
             "Category is a required field.")
     private CategoryDto category;
 
