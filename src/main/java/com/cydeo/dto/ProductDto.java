6e7191b803fa39a990384ae4f19136d7a9d93b85
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-07-01 06:54:12 -0400

    validation annotations added

--- src/main/java/com/cydeo/dto/ProductDto.java
+++ src/main/java/com/cydeo/dto/ProductDto.java
@@ -3,6 +3,9 @@ package com.cydeo.dto;
 import com.cydeo.enums.ProductUnit;
 import lombok.*;
 
+
+import javax.validation.constraints.*;
+
 @Getter
 @Setter
 @ToString
@@ -11,10 +14,24 @@ import lombok.*;
 public class ProductDto {
 
     private Long id;
+
+    @NotBlank(message = "Product Name is required field.")
+    @Size(min = 2,max=100)
     private String name;
+
     private Integer quantityInStock;
+
+    @NotNull(message ="Low Limit Alert is a required field."
+    + " Low Limit Alert should be at least 1.")
     private Integer lowLimitAlert;
+
+    @NotNull(message = "Please select a Product Unit."+
+             "Product Unit is a required field.")
     private ProductUnit productUnit;
+
+
+    @NotNull(message = "Please select a category."+
+            "Category is a required field.")
     private CategoryDto category;
 
 }
