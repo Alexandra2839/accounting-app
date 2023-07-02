b7c95580ae62e36a583cf9491b2c3f4e565c2bc6
Anna Pierre-Louis <anitataksa3440@gmail.com>
2023-07-02 14:14:41 -0400

    L-56 created Dashboard service and impl, created dash controller with get method to display the dashboard and its parts

---
+++ src/main/java/com/cydeo/service/impl/DashboardServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.service.DashboardService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class DashboardServiceImpl implements DashboardService {
+}
