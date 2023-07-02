badf2aaac3f74ef1b0a82bfb21f2490aad6f00b0
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-07-02 13:39:16 -0400

    validation added

--- src/main/java/com/cydeo/dto/CategoryDto.java
+++ src/main/java/com/cydeo/dto/CategoryDto.java
@@ -2,6 +2,9 @@ package com.cydeo.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @ToString
@@ -10,7 +13,11 @@ import lombok.*;
 public class CategoryDto {
 
     private Long id;
+
+    @NotBlank(message = "Description is a required field.")
+    @Size(min = 2, max = 100, message = "Description should have 2-100 characters long.")
     private String description;
+
     private CompanyDto company;
     private boolean hasProduct;
 }
