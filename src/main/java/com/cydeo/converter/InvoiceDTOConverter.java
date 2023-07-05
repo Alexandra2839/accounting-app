d3900c22af32efbea18d03baf2e0290a7cdcba3c
lichfb <lichfb@gmail.com>
2023-07-05 16:53:42 -0400

    InvoiceDTO and InvoiceProductDTo improved

--- src/main/java/com/cydeo/converter/InvoiceDTOConverter.java
+++ src/main/java/com/cydeo/converter/InvoiceDTOConverter.java
@@ -5,7 +5,9 @@ import com.cydeo.service.InvoiceService;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class InvoiceDTOConverter implements Converter<String, InvoiceDto> {
     private final InvoiceService invoiceService;
 
@@ -15,6 +17,11 @@ public class InvoiceDTOConverter implements Converter<String, InvoiceDto> {
 
     @Override
     public InvoiceDto convert(String source) {
+
+        if (source == null || source.equals("")) {
+            return null;
+        }
+
         return invoiceService.findById(Long.parseLong(source));
     }
 }
--- src/main/java/com/cydeo/converter/InvoiceProductDTOConverter.java
+++ src/main/java/com/cydeo/converter/InvoiceProductDTOConverter.java
@@ -3,7 +3,9 @@ package com.cydeo.converter;
 import com.cydeo.dto.InvoiceProductDto;
 import com.cydeo.service.InvoiceProductService;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class InvoiceProductDTOConverter implements Converter<String, InvoiceProductDto> {
 
     private final InvoiceProductService invoiceProductService;
