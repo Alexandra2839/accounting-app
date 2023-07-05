8690dead8ad3ae90372b75f4534902f3716e542e
Sasha <alexandra.aslobodian@gmail.com>
2023-07-05 18:56:32 -0400

    added /profitLossData endpoint

--- src/main/java/com/cydeo/controller/ReportingController.java
+++ src/main/java/com/cydeo/controller/ReportingController.java
@@ -1,9 +1,19 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/reports")
 public class ReportingController {
+
+    @GetMapping("/profitLossData")
+    public String getProfitLossReport(Model model){
+
+
+
+        return "report/profit-loss-report";
+    }
 }
