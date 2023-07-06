f8f519ee297806a945532beea47d1a6888bbaabf
Sasha <alexandra.aslobodian@gmail.com>
2023-07-06 10:53:04 -0400

    removed InvoiceRepository from InvoiceProductServiceImpl

--- src/main/java/com/cydeo/service/impl/InvoiceProductServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/InvoiceProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.InvoiceDto;
 import com.cydeo.dto.InvoiceProductDto;
 import com.cydeo.entity.Invoice;
 import com.cydeo.entity.InvoiceProduct;
@@ -7,26 +8,27 @@ import com.cydeo.enums.InvoiceStatus;
 import com.cydeo.enums.InvoiceType;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.InvoiceProductRepository;
-import com.cydeo.repository.InvoiceRepository;
 import com.cydeo.service.InvoiceProductService;
+import com.cydeo.service.InvoiceService;
 import com.cydeo.service.SecurityService;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class InvoiceProductServiceImpl implements InvoiceProductService {
     private final InvoiceProductRepository invoiceProductRepository;
-    private final InvoiceRepository invoiceRepository;
+    private final InvoiceService invoiceService;
     private final MapperUtil mapperUtil;
-
     private final SecurityService securityService;
 
-    public InvoiceProductServiceImpl(InvoiceProductRepository invoiceProductRepository, InvoiceRepository invoiceRepository, MapperUtil mapperUtil, SecurityService securityService) {
+    public InvoiceProductServiceImpl(InvoiceProductRepository invoiceProductRepository, @Lazy InvoiceService invoiceService, MapperUtil mapperUtil, SecurityService securityService) {
         this.invoiceProductRepository = invoiceProductRepository;
-        this.invoiceRepository = invoiceRepository;
+        this.invoiceService = invoiceService;
         this.mapperUtil = mapperUtil;
         this.securityService = securityService;
     }
@@ -47,7 +49,8 @@ public class InvoiceProductServiceImpl implements InvoiceProductService {
 
     @Override
     public InvoiceProductDto save(InvoiceProductDto invoiceProductDto, Long id) {
-        Invoice invoice = invoiceRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No such invoice in the system"));
+        InvoiceDto invoiceDto = invoiceService.findById(id);
+        Invoice invoice = mapperUtil.convert(invoiceDto, new Invoice());
 
         InvoiceProduct invoiceProduct = mapperUtil.convert(invoiceProductDto, new InvoiceProduct());
 
@@ -60,7 +63,7 @@ public class InvoiceProductServiceImpl implements InvoiceProductService {
 
     @Override
     public InvoiceProductDto deleteInvoiceProduct(Long invoiceId, Long productId) {
-        InvoiceProduct invoiceProduct = invoiceProductRepository.findByInvoiceIdAndId(invoiceId,productId);
+        InvoiceProduct invoiceProduct = invoiceProductRepository.findByInvoiceIdAndId(invoiceId, productId);
         invoiceProduct.setIsDeleted(true);
         invoiceProductRepository.save(invoiceProduct);
         return mapperUtil.convert(invoiceProduct, new InvoiceProductDto());
@@ -82,17 +85,22 @@ public class InvoiceProductServiceImpl implements InvoiceProductService {
         BigDecimal tax = invoiceProductDTO.getTax();
         BigDecimal totalAmountWithoutTax = invoiceProductDTO.getPrice().multiply(quantity);
         BigDecimal totalAmountWithTax = totalAmountWithoutTax.add(totalAmountWithoutTax.multiply(tax).divide(new BigDecimal(100)));
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        String formattedTotalAmountWithTax = decimalFormat.format(totalAmountWithTax);
+        BigDecimal totalAmountWithTaxValue = new BigDecimal(formattedTotalAmountWithTax);
 
-        invoiceProductDTO.setTotal(totalAmountWithTax);
+        invoiceProductDTO.setTotal(totalAmountWithTaxValue);
 
         return invoiceProductDTO;
     }
+
     @Override
     public boolean isStockNotEnough(InvoiceProductDto invoiceProductDTO) {
         if (invoiceProductDTO.getQuantity() == null || invoiceProductDTO.getProduct().getQuantityInStock() == null)
             return true;
         return invoiceProductDTO.getQuantity() > invoiceProductDTO.getProduct().getQuantityInStock();
     }
+
     @Override
     public Map<String, BigDecimal> listMonthlyProfitLoss() {
 
