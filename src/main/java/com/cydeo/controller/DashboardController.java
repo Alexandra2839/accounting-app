f105697a338789b4bfa8bda3a84e9548b0856b54
Sasha <alexandra.aslobodian@gmail.com>
2023-07-06 11:50:46 -0400

    updated to call methods from InvoiceService and InvoiceProductService

--- src/main/java/com/cydeo/controller/DashboardController.java
+++ src/main/java/com/cydeo/controller/DashboardController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
 import com.cydeo.service.DashboardService;
+import com.cydeo.service.InvoiceProductService;
+import com.cydeo.service.InvoiceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,21 +16,26 @@ import java.util.Map;
 public class DashboardController {
 
     private final DashboardService dashboardService;
+    private final InvoiceService invoiceService;
+    private final InvoiceProductService invoiceProductService;
 
-    public DashboardController(DashboardService dashboardService) {
+    public DashboardController(DashboardService dashboardService, InvoiceService invoiceService, InvoiceProductService invoiceProductService) {
         this.dashboardService = dashboardService;
+        this.invoiceService = invoiceService;
+        this.invoiceProductService = invoiceProductService;
     }
+
 
     @GetMapping
     public String getDash(Model model) {
 
         Map<String, BigDecimal> summaryNumbers = Map.of(
-                "totalCost", dashboardService.calculateTotalCost(),
-                "totalSales", dashboardService.calculateTotalSales(),
-                "profitLoss", dashboardService.calculateTotalProfitLoss()
+                "totalCost", invoiceService.calculateTotalCost(),
+                "totalSales", invoiceService.calculateTotalSales(),
+                "profitLoss", invoiceProductService.calculateTotalProfitLoss()
         );
         model.addAttribute("summaryNumbers", summaryNumbers);
-        model.addAttribute("invoices", dashboardService.list3LastApprovedInvoices());
+        model.addAttribute("invoices", invoiceService.list3LastApprovedInvoices());
         model.addAttribute("exchangeRates", dashboardService.getRates());
         model.addAttribute("title", "Cydeo Accounting-Dashboard");
         return "dashboard";
