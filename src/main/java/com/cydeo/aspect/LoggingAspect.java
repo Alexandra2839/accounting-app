79c2f8d118572312661c2422a57b253152fc5391
Sasha <alexandra.aslobodian@gmail.com>
2023-07-06 21:42:57 -0400

    added @Pointcut and @AfterThrowing for whole project

--- src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,11 +1,26 @@
 package com.cydeo.aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
 public class LoggingAspect {
+
+    @Pointcut("execution(* com.cydeo.*.*.*(..))")
+    public void anyMethodWithinProject() {}
+
+    @AfterThrowing(pointcut = "anyMethodWithinProject()", throwing = "exception")
+    public void afterReturningAnyProjectAndTaskControllerAdvice(JoinPoint joinPoint, Exception exception) {
+        log.info("Method: {}, Exception: {}, Message: {}"
+                , joinPoint.getSignature().toShortString()
+                , exception.getClass().getName()
+                , exception.getMessage());
+    }
+
 }
