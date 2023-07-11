03988de5f9d5c7dff88cba6840de0af9121b4bfb
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-07-11 00:59:49 -0400

    NoSuchElementException replaced to ProductNotFoundException

--- src/main/java/com/cydeo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProductServiceImpl.java
@@ -4,6 +4,7 @@ package com.cydeo.service.impl;
 import com.cydeo.dto.CompanyDto;
 import com.cydeo.dto.ProductDto;
 import com.cydeo.entity.Product;
+import com.cydeo.exception.ProductNotFoundException;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.ProductRepository;
 import com.cydeo.service.CompanyService;
@@ -11,7 +12,6 @@ import com.cydeo.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -53,7 +53,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductDto findProductById(Long id) {
         Product product = productRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Product has not been found"));
+                .orElseThrow(() -> new ProductNotFoundException("Product with this id "+id+" has not been found"));
 
         return mapper.convert(product, new ProductDto());
     }
@@ -71,7 +71,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductDto deleteProduct(ProductDto productDto) {
         Product product = productRepository.findById(productDto.getId())
-                .orElseThrow(() -> new NoSuchElementException("Product has not been found"));
+                .orElseThrow(() -> new ProductNotFoundException("Product has not been found"));
         product.setIsDeleted(true);
         product.setName(productDto.getName() + " / " + productDto.getId());
         productRepository.save(product);
