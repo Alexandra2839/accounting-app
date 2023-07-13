8415c90815037577b2c09d42577126e538bb1bbc
Sasha <alexandra.aslobodian@gmail.com>
2023-07-12 21:54:47 -0400

    put @Spy for MapperUtil and updated findById and update tests

--- src/test/java/com/cydeo/service/impl/unit/UserServiceImplUnitTest.java
+++ src/test/java/com/cydeo/service/impl/unit/UserServiceImplUnitTest.java
@@ -8,12 +8,13 @@ import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.SecurityService;
 import com.cydeo.service.impl.UserServiceImpl;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
@@ -28,11 +29,10 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplUnitTest {
 
+    @Spy
+    static MapperUtil mapperUtil = new MapperUtil(new ModelMapper());
     @Mock
     private UserRepository userRepository;
-    @Mock
-    private MapperUtil mapperUtil;
-
     @Mock
     private PasswordEncoder passwordEncoder;
     @Mock
@@ -51,23 +51,27 @@ class UserServiceImplUnitTest {
 
     @Test
     public void should_return_user_by_id_if_exists() {
+        UserDto userDto = TestDocumentInitializer.getUser("admin");
+        User user = mapperUtil.convert(userDto, new User());
 
-        User user = TestDocumentInitializer.getUserEntity("admin");
-        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-        assertThat(userService.findById(1L)).isEqualTo(mapperUtil.convert(user, new UserDto()));
+        when(userRepository.findById(userDto.getId())).thenReturn(Optional.of(user));
+
+        UserDto actualUser = userService.findById(1L);
+        assertThat(actualUser).usingRecursiveComparison().ignoringFields("password", "confirmPassword", "company.id").isEqualTo(userDto);
 
     }
 
     @Test
     public void should_update_user_and_return_updated_user() {
-        User user = TestDocumentInitializer.getUserEntity("role");
         UserDto userDto = TestDocumentInitializer.getUser("role");
-        when(userRepository.findByUsername(anyString())).thenReturn(user);
-        when(mapperUtil.convert(any(UserDto.class), any(User.class))).thenReturn(user);
-        when(userRepository.save(user)).thenReturn(user);
-        when(mapperUtil.convert(any(User.class), any(UserDto.class))).thenReturn(userDto);
         userDto.setUsername("JohnSmith@test.com");
         userDto.setFirstname("John");
+        User user = mapperUtil.convert(userDto, new User());
+
+        when(userRepository.findByUsername(anyString())).thenReturn(user);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+
         UserDto updatedUser = userService.update(userDto);
         assertThat(updatedUser.getUsername()).isEqualTo("JohnSmith@test.com");
     }
@@ -81,7 +85,6 @@ class UserServiceImplUnitTest {
         given(securityService.getLoggedInUser()).willReturn(userDto);
         given(userRepository.countByCompanyTitleAndRoleDescription(anyString(), anyString())).willReturn(2);
         given(userRepository.findAllByRoleDescriptionOrderByCompanyTitleAscRoleDescriptionAsc(anyString())).willReturn(List.of(user, user2));
-        given(mapperUtil.convert(any(User.class), any(UserDto.class))).willReturn(TestDocumentInitializer.getUser("Admin"));
 
         List<UserDto> userList = userService.listAllUsers();
 
@@ -97,7 +100,6 @@ class UserServiceImplUnitTest {
         given(securityService.getLoggedInUser()).willReturn(userDto);
         given(userRepository.countByCompanyTitleAndRoleDescription(anyString(), anyString())).willReturn(2);
         given(userRepository.findAllByCompanyTitleOrderByCompanyTitleAscRoleDescriptionAsc(anyString())).willReturn(List.of(user));
-        given(mapperUtil.convert(any(User.class), any(UserDto.class))).willReturn(TestDocumentInitializer.getUser("Admin"));
 
         List<UserDto> userList = userService.listAllUsers();
 
@@ -110,10 +112,8 @@ class UserServiceImplUnitTest {
         User user = TestDocumentInitializer.getUserEntity("Manager");
         UserDto userDto = TestDocumentInitializer.getUser("Manager");
 
-        when(mapperUtil.convert(any(UserDto.class), any(User.class))).thenReturn(user);
         when(passwordEncoder.encode(anyString())).thenReturn(user.getPassword());
         when(userRepository.save(any(User.class))).thenReturn(user);
-        when(mapperUtil.convert(any(User.class), any(UserDto.class))).thenReturn(userDto);
 
         UserDto savedUser = userService.save(userDto);
 
@@ -140,18 +140,17 @@ class UserServiceImplUnitTest {
         assertThat(userService.isEmailExist(userDto)).isTrue();
     }
 
-    @DisplayName("JUnit test for delete method when id does not exist")
     @Test
-    public void should_throw_exception_when_user_id_to_delete_does_not_exist(){
+    public void should_throw_exception_when_user_id_to_delete_does_not_exist() {
         when(userRepository.findById(1L)).thenReturn(Optional.empty());
 
         Throwable throwable = catchThrowable(() -> userService.delete(1L));
         assertThat(throwable).isInstanceOf(UserNotFoundException.class);
     }
 
-    @DisplayName("JUnit test for delete method")
+
     @Test
-    public void should_soft_delete_user(){
+    public void should_soft_delete_user() {
         User user = TestDocumentInitializer.getUserEntity("Manager");
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 
@@ -162,10 +161,10 @@ class UserServiceImplUnitTest {
     }
 
     @Test
-    public void should_set_only_admin_if_condition_met(){
+    public void should_set_only_admin_if_condition_met() {
         UserDto userDto = TestDocumentInitializer.getUser("Admin");
 
-        when(userRepository.countByCompanyTitleAndRoleDescription(anyString(),anyString())).thenReturn(1);
+        when(userRepository.countByCompanyTitleAndRoleDescription(anyString(), anyString())).thenReturn(1);
 
         UserDto updatedUser = userService.setOnlyAdmin(userDto);
         assertThat(updatedUser.isOnlyAdmin()).isTrue();
